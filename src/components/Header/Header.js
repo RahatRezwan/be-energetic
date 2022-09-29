ec9c491bb4dd5fb400e406e5bdf31e8072ca8535
Kazi Rahat <kazirahat1007@outlook.com>
2022-09-29 08:34:32 +0600

    update site logo and favicon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,12 @@
 import React from "react";
+import logo from "../../logo.svg";
 import "./Header.css";
 const Header = () => {
-    return <div></div>;
+    return (
+        <div className="header">
+            <img src={logo} alt="" />
+        </div>
+    );
 };
 
 export default Header;
