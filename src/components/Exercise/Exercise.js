86ef204f334e5cdfd16109722294be1123894be7
Kazi Rahat <kazirahat1007@outlook.com>
2022-09-29 08:07:36 +0600

    display exercise data in dom

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -4,7 +4,15 @@ import "./Exercise.css";
 const Exercise = ({ exercise }) => {
     console.log(exercise);
     const { img, name, details, time, Age } = exercise;
-    return <div></div>;
+    return (
+        <div>
+            <img src={img} alt="" />
+            <h5>{name}</h5>
+            <p>{details}</p>
+            <p>{time}</p>
+            <p>For Age: {Age}</p>
+        </div>
+    );
 };
 
 export default Exercise;
