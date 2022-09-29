5c42dafff94a5b371c06095782830a6f89d3966b
Kazi Rahat <kazirahat1007@outlook.com>
2022-09-29 12:49:02 +0600

    make website responsive

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,7 +1,19 @@
 import React from "react";
 import "./Summary.css";
 const Summary = () => {
-    return <div>this is summary</div>;
+    return (
+        <div>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <h4>Kazi Rahat Rezwan</h4>
+                <p>
+                    <small>Reactjs Developer</small>
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default Summary;
