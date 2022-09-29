16b77a89d8ed9fcc97660de49f6f20d4737ee126
Kazi Rahat <kazirahat1007@outlook.com>
2022-09-29 07:55:57 +0600

    Create Main and Exercise Components

---
+++ src/components/Exercise/Exercise.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Exercise.css";
+
+const Exercise = ({ exercise }) => {
+    console.log(exercise);
+    return <div></div>;
+};
+
+export default Exercise;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import Exercise from "../Exercise/Exercise";
+import "./Main.css";
+
+const Main = () => {
+    const [exercises, setExercises] = useState([]);
+    useEffect(() => {
+        fetch("exercise.json")
+            .then((res) => res.json())
+            .then((data) => setExercises(data));
+    }, []);
+    return (
+        <div>
+            <div>
+                {exercises.map((exercise) => (
+                    <Exercise key={exercise.id} exercise={exercise}></Exercise>
+                ))}
+            </div>
+            <div></div>
+        </div>
+    );
+};
+
+export default Main;
