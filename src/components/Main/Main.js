c7b1cdf7cafa2c2c7ab8c316decb4313cc99c222
Kazi Rahat <kazirahat1007@outlook.com>
2022-10-10 07:23:53 +0600

    fix exercise time calculation

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,67 +6,68 @@ import Summary from "../Summary/Summary";
 import "./Main.css";
 
 const Main = (props) => {
-    const { notify } = props;
-    /* create state for fetching exercise */
-    const [exercises, setExercises] = useState([]);
-    /* State for exercisetime */
-    const [timeTaken, setTimeTaken] = useState(0);
-    /* State for question */
-    const [questions, setQuestions] = useState([]);
+  const { notify } = props;
+  /* create state for fetching exercise */
+  const [exercises, setExercises] = useState([]);
+  /* State for exercisetime */
+  const [timeTaken, setTimeTaken] = useState(0);
+  /* State for question */
+  const [questions, setQuestions] = useState([]);
 
-    useEffect(() => {
-        fetch("exercise.json")
-            .then((res) => res.json())
-            .then((data) => setExercises(data));
-    }, []);
+  useEffect(() => {
+    fetch("exercise.json")
+      .then((res) => res.json())
+      .then((data) => setExercises(data));
+  }, []);
 
-    /* useeffect for question */
-    useEffect(() => {
-        fetch("questions.json")
-            .then((res) => res.json())
-            .then((data) => setQuestions(data));
-    }, []);
+  /* useeffect for question */
+  useEffect(() => {
+    fetch("questions.json")
+      .then((res) => res.json())
+      .then((data) => setQuestions(data));
+  }, []);
 
-    useEffect(() => {
-        const storedTime = getStoredTime("exercise");
-        let savedTime = 0;
-        for (const id in storedTime) {
-            savedTime = savedTime + storedTime[id];
-        }
-        setTimeTaken(savedTime);
-    }, [exercises]);
+  useEffect(() => {
+    const storedTime = getStoredTime("exercise");
+    let savedTime = 0;
+    for (const id in storedTime) {
+      savedTime = savedTime + storedTime[id];
+    }
+    setTimeTaken(savedTime);
+  }, [exercises]);
 
-    const handleAddToList = (currentExercise) => {
-        const time = currentExercise.time;
-        setTimeTaken(timeTaken + time);
-        addToLocalDb("exercise", currentExercise.id, timeTaken);
-    };
-    return (
-        <div className="main-section">
-            <div className="main-container">
-                <div className="exercise-container">
-                    <h2>Select Your Exercise</h2>
-                    <div className="exercise-div">
-                        {exercises.map((exercise) => (
-                            <Exercise
-                                key={exercise.id}
-                                exercise={exercise}
-                                handleAddToList={handleAddToList}
-                            ></Exercise>
-                        ))}
-                    </div>
-                </div>
-                <div className="summary-div">
-                    <Summary notify={notify} timeTaken={timeTaken}></Summary>
-                </div>
-            </div>
-            <div className="que-container">
-                {questions.map((que) => (
-                    <Questions key={que.id} que={que}></Questions>
-                ))}
-            </div>
+  const handleAddToList = (currentExercise) => {
+    const time = currentExercise.time;
+    addToLocalDb("exercise", currentExercise.id, time);
+    setTimeTaken((prev) => prev + time);
+  };
+
+  return (
+    <div className="main-section">
+      <div className="main-container">
+        <div className="exercise-container">
+          <h2>Select Your Exercise</h2>
+          <div className="exercise-div">
+            {exercises.map((exercise) => (
+              <Exercise
+                key={exercise.id}
+                exercise={exercise}
+                handleAddToList={handleAddToList}
+              ></Exercise>
+            ))}
+          </div>
         </div>
-    );
+        <div className="summary-div">
+          <Summary notify={notify} timeTaken={timeTaken}></Summary>
+        </div>
+      </div>
+      <div className="que-container">
+        {questions.map((que) => (
+          <Questions key={que.id} que={que}></Questions>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Main;
--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -5,99 +5,99 @@ import Interval from "../Interval/Interval";
 import { addIntervalToLocalDb, getStoredTime } from "../../utilities/utilities";
 import { ToastContainer } from "react-toastify";
 const Summary = (props) => {
-    const { timeTaken, notify } = props;
-    /* Get Breaktime state */
-    const [breaks, setBreaks] = useState([]);
+  const { timeTaken, notify } = props;
+  /* Get Breaktime state */
+  const [breaks, setBreaks] = useState([]);
 
-    /* handle Breaktime State */
-    const [intervalTime, setintervalTime] = useState(0);
+  /* handle Breaktime State */
+  const [intervalTime, setintervalTime] = useState(0);
 
-    useEffect(() => {
-        fetch("breaktime.json")
-            .then((res) => res.json())
-            .then((data) => setBreaks(data));
-    }, []);
+  useEffect(() => {
+    fetch("breaktime.json")
+      .then((res) => res.json())
+      .then((data) => setBreaks(data));
+  }, []);
 
-    useEffect(() => {
-        const storedTime = getStoredTime("Break");
-        let savedTime = 0;
-        for (const id in storedTime) {
-            savedTime = savedTime + storedTime[id];
-        }
-        setintervalTime(savedTime);
-    }, [breaks]);
+  useEffect(() => {
+    const storedTime = getStoredTime("Break");
+    let savedTime = 0;
+    for (const id in storedTime) {
+      savedTime = savedTime + storedTime[id];
+    }
+    setintervalTime(savedTime);
+  }, [breaks]);
 
-    const handleBreakTime = (interval) => {
-        setintervalTime(interval.num);
-        addIntervalToLocalDb("Break", interval.id, interval.num);
-    };
-    return (
-        <div className="summary">
-            <div className="personalInfo">
-                <div className="intro">
-                    <img src={myPic} alt="" />
-                    <div>
-                        <h3>Kazi Rahat Rezwan</h3>
-                        <small>Reactjs Developer</small>
-                    </div>
-                </div>
-                <div className="more-info common">
-                    <div>
-                        <h3>75kg</h3>
-                        <p>
-                            <small>Weight</small>
-                        </p>
-                    </div>
-                    <div>
-                        <h3>5.8</h3>
-                        <p>
-                            <small>Height</small>
-                        </p>
-                    </div>
-                    <div>
-                        <h3>25yrs</h3>
-                        <p>
-                            <small>Age</small>
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            {/* BreakTime Section */}
-            <div className="breakTime">
-                <h3>Take A Break</h3>
-                <div className="break-div common">
-                    {breaks.map((interval) => (
-                        <Interval
-                            key={interval.id}
-                            interval={interval}
-                            handleBreakTime={handleBreakTime}
-                        ></Interval>
-                    ))}
-                </div>
-            </div>
-
-            {/* Exercise Details Section */}
-            <div className="exerciseDetails">
-                <h3>Exercise Details</h3>
-                <div className="common exercise-time">
-                    <h3>Exercise Time</h3>
-                    <p>{timeTaken} Minutes</p>
-                </div>
-                <div className="common exercise-time">
-                    <h3>Break Time</h3>
-                    <p>{intervalTime} Minutes</p>
-                </div>
-            </div>
-
-            <div className="activity-btn-div">
-                <button onClick={notify} className="activity-btn">
-                    Activity Complete
-                </button>
-                <ToastContainer></ToastContainer>
-            </div>
+  const handleBreakTime = (interval) => {
+    setintervalTime(interval.num);
+    addIntervalToLocalDb("Break", interval.id, interval.num);
+  };
+  return (
+    <div className="summary">
+      <div className="personalInfo">
+        <div className="intro">
+          <img src={myPic} alt="" />
+          <div>
+            <h3>Kazi Rahat Rezwan</h3>
+            <small>Reactjs Developer</small>
+          </div>
         </div>
-    );
+        <div className="more-info common">
+          <div>
+            <h3>75kg</h3>
+            <p>
+              <small>Weight</small>
+            </p>
+          </div>
+          <div>
+            <h3>5.8</h3>
+            <p>
+              <small>Height</small>
+            </p>
+          </div>
+          <div>
+            <h3>25yrs</h3>
+            <p>
+              <small>Age</small>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* BreakTime Section */}
+      <div className="breakTime">
+        <h3>Take A Break</h3>
+        <div className="break-div common">
+          {breaks.map((interval) => (
+            <Interval
+              key={interval.id}
+              interval={interval}
+              handleBreakTime={handleBreakTime}
+            ></Interval>
+          ))}
+        </div>
+      </div>
+
+      {/* Exercise Details Section */}
+      <div className="exerciseDetails">
+        <h3>Exercise Details</h3>
+        <div className="common exercise-time">
+          <h3>Exercise Time</h3>
+          <p>{timeTaken} Minutes</p>
+        </div>
+        <div className="common exercise-time">
+          <h3>Break Time</h3>
+          <p>{intervalTime} Minutes</p>
+        </div>
+      </div>
+
+      <div className="activity-btn-div">
+        <button onClick={notify} className="activity-btn">
+          Activity Complete
+        </button>
+        <ToastContainer></ToastContainer>
+      </div>
+    </div>
+  );
 };
 
 export default Summary;
